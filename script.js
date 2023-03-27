9a3ad55cde77a292261b264e7d6e7ddf5ec421a2
Rajupalem Hanumat Srinivas <sharma.srinivas21@gmail.com>
2023-03-27 23:46:01 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function myFunction(){
+
+document.getElementById("chat").innerHTML = "Please mail me to my gmail :sharma.srnivas21@gmail.com";
+
+    
+}
+function show() {
+ 
+    /* Access image by id and change
+    the display property to block*/
+    document.getElementById('image1')
+            .style.display = "block";
+
+    document.getElementById('btn1')
+            .style.display = "none";}
